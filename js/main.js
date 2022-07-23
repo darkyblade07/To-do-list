4e4ccc888707b988d8156f173b362c3d7e49b16a
Santiago Marin <101464523+darkyblade07@users.noreply.github.com>
2022-07-23 09:43:06 -0600

    v.2

--- js/main.js
+++ js/main.js
@@ -1,27 +1,46 @@
-const input = document.getElementById('inputIn');
-const saveBtn = document.getElementById('submitBtn');
-let myToDo = []
 
-saveBtn.addEventListener("click", save());
+window.onload = function () {
 
-function save() {
-   console.log('working')
-    myToDo.push(input.value)
-    console.log('saving...')
-    render()
-    input.value = ""
-}
+    const saveBtn = document.getElementById('submitBtn');
+    const ul = document.getElementById('ulIn');
+    const input = document.getElementById('inputIn');
+    const clear = document.getElementById('clearBtn');
+    const text = document.getElementById('congratsText');
 
-function render() {
-    const ul = document.createElement('ul')
-    let list = ""
-    myToDo.forEach(element => {
-        list += `<li>
+    let myToDo = []
+
+    saveBtn.addEventListener("click", save);
+    clear.addEventListener("click", clearLi);
+
+
+    function save() {
+        if (input.value === "") {
+
+        } else {
+            myToDo.push(input.value)
+            render()
+            input.value = ""
+        }
+
+    }
+
+    function render() {
+        let list = ""
+        ul.innerHTML = ""
+        myToDo.forEach(element => {
+            list += `<li>
         ${element}
         </li>`
-    });
+        });
 
-    ul.innerHTML = list
+        ul.innerHTML = list
+
+    }
+
+    function clearLi() {
+
+        ul.innerHTML = ""
+        text.innerHTML = `Congrats for finishing your day! Keep it up`
+    }
 
 }
-
